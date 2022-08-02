3e082fd46dfde0312c11e4b8a8b0fc5dc9b6d30f
merupooo <sae.fujimori@rakus-partners.co.jp>
2022-08-02 16:55:41 +0900

    AdministratorContorollerクラスをコミット

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,30 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/administrator")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorservice;
+
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		InsertAdministratorForm insertAdministratorForm = new InsertAdministratorForm();
+		return insertAdministratorForm;
+
+	}
+
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+
+	}
+}
