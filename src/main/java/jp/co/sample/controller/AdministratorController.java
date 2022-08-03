4866fc0a3871a2c18b2cfa1d8fbef5c417c02a1d
merupooo <sae.fujimori@rakus-partners.co.jp>
2022-08-03 11:16:44 +0900

    AdministratorControllerクラスにメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -25,6 +28,15 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+
+	}
+
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form, RedirectAttributes redirectAttributes) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorservice.insert(administrator);
+		return "/";
 
 	}
 }
